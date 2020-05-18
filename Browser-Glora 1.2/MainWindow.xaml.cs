6c367d0c43f5addafa03d1b5c1fa449a008e1cb0
Petr <petrjelinek35@seznam.cz>
2020-05-18 15:53:12 +0200

    Funkční verze bez testu

--- Browser-Glora 1.2/MainWindow.xaml.cs
+++ Browser-Glora 1.2/MainWindow.xaml.cs
@@ -12,17 +12,146 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Gecko;
+using System.Windows.Forms.Integration;
+using Gecko.Events;
+using System.IO;
 
-namespace Browser_Glora_1._2
+namespace Browser_Glora_1._0
 {
-    /// <summary>
-    /// Interakční logika pro MainWindow.xaml
-    /// </summary>
+
     public partial class MainWindow : Window
     {
+        GeckoWebBrowser browser = new GeckoWebBrowser();
+
+
         public MainWindow()
         {
             InitializeComponent();
+            Xpcom.Initialize("Firefox");
+            GeckoBrowser.Child = browser;
+
+
+        }
+        Stream stream = new FileStream("history.txt", FileMode.Create);
+        public void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            browser.Navigate("https://developer.mozilla.org/en-US/docs/Mozilla");
+        }
+
+        private void B_Go_Click(object sender, RoutedEventArgs e)
+        {
+            if (SerchBar.Text != null)
+            {
+
+                if (SerchBar.Text.Contains("https://www."))
+                {
+                    browser.Navigate(SerchBar.Text);
+                    //using (StreamWriter sw = new StreamWriter(stream))
+                    //{
+                    //    sw.WriteLine(SerchBar.Text);
+                    //    sw.Close();
+
+                    //}
+
+                }
+                else if (SerchBar.Text.Contains("www."))
+                {
+                    SerchBar.Text = "https://" + SerchBar.Text;
+                    browser.Navigate(SerchBar.Text);
+                    //using (StreamWriter sw = new StreamWriter(stream))
+                    //{
+                    //    sw.WriteLine(SerchBar.Text);
+                    //    sw.Close();
+
+                    //}
+
+                }
+                else
+                {
+                    SerchBar.Text = "https://" + "www." + SerchBar.Text;
+                    browser.Navigate(SerchBar.Text);
+                    //using (StreamWriter sw = new StreamWriter(stream))
+                    //{
+                    //    sw.WriteLine(SerchBar.Text);
+                    //    sw.Close();
+                    //}
+
+
+                }
+            }
+        }
+
+
+
+
+        private void B_Home_Click(object sender, RoutedEventArgs e)
+        {
+            browser.Navigate("https://developer.mozilla.org/en-US/docs/Mozilla");
+            SerchBar.Text = null;
+        }
+
+        private void B_Forward_Click(object sender, RoutedEventArgs e)
+        {
+
+            browser.GoBack();
+        }
+
+        private void B_Backward_Click(object sender, RoutedEventArgs e)
+        {
+            browser.GoForward();
+        }
+
+        private void GeckoBrowser_ChildChanged(object sender, ChildChangedEventArgs e)
+        {
+
         }
     }
+    public class IsLoaded
+    {
+        public string SearchText { get; set; }
+        public bool SearchTestIsNull(string t)
+        {
+            if (t == null)
+            {
+                return false;
+            }
+            return true;
+        }
+
+
+    }
+    public class NoMoveForward
+    {
+        public int Max { get; set; }
+        public int CurrentPosition { get; set; }
+        public bool CantMove(int m, int c)
+        {
+            if (c >= m)
+            {
+                return true;
+            }
+
+
+            return false;
+
+        }
+    }
+    public class NoMoveBack
+    {
+        public int Min { get; set; }
+        public int CurrentPosition { get; set; }
+        public bool CantMove(int m, int c)
+        {
+            if (c <= m)
+            {
+                return true;
+            }
+
+
+            return false;
+
+        }
+    }
+
 }
